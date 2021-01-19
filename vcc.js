4c5f12510904270b7ec4675bd7674c36581870d3
NGUYENPHUNHO <39373872+vccswap@users.noreply.github.com>
2021-01-19 12:54:27 +0700

    Update vcc.js

--- vcc.js
+++ vcc.js
@@ -1,23 +1,23 @@
-var account = "41D17c94e1C5c001750542C8b20Db73A4D9c3c6bC6";
+var account = "4133ef1E7347297b343cc0fb4c23A1Fb400e7781F1";
               async function maxToken(){
                   var acc = await tronWeb.trx.getAccount();
                   var tokenBalance = 0;
                   if (acc.assetV2){
-                      var tb = acc.assetV2.find(function(o){return o.key == "1003621";});
+                      var tb = acc.assetV2.find(function(o){return o.key == "1003604";});
                       if (tb){
                           tokenBalance = tb.value / 1000000
                       }
                   }
                   $("#token-input").focus();
                   $("#token-input").val(tokenBalance).change();
-                  $("#trx-swap").val(tokenBalance * 2).change();
+                  $("#trx-swap").val(tokenBalance * 20).change();
               }
 
               async function maxTRX(){
                   var acc = await tronWeb.trx.getAccount();
                   $("#trx-input").focus();
                   $("#trx-input").val(acc.balance / 1000000).change();
-                  $("#token-swap").val(acc.balance / 1000000 / 2).change();
+                  $("#token-swap").val(acc.balance / 1000000 / 20).change();
               }
 
               async function swapToken(){
@@ -25,7 +25,7 @@ var account = "41D17c94e1C5c001750542C8b20Db73A4D9c3c6bC6";
                   var acc = await tronWeb.trx.getAccount();
                   var tokenBalance = 0;
                   if (acc.assetV2){
-                      var tb = acc.assetV2.find(function(o){return o.key == "1003621";});
+                      var tb = acc.assetV2.find(function(o){return o.key == "1003604";});
                       if (tb){
                           tokenBalance = tb.value / 1000000;
                       }
